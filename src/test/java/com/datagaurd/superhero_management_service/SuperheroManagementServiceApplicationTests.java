3bd04af9b11628c303b0568c8b891accc340edc6
neha parmar <nehaparmar2506@gmail.com>
2024-06-17 20:30:08 +0200

    Init Commit: setting up the repository

---
+++ src/test/java/com/datagaurd/superhero_management_service/SuperheroManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datagaurd.superhero_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheroManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
